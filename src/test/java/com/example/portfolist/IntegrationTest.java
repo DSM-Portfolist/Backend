d2e3247c190b842332aa98daf6b4e220182baa84
dohyeon5626 <kodohyeon71@gmail.com>
2021-09-17 13:47:41 +0900

    Add test code base

---
+++ src/test/java/com/example/portfolist/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.example.portfolist;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PortfolistApplication.class)
+@AutoConfigureMockMvc
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+public class IntegrationTest {
+}
