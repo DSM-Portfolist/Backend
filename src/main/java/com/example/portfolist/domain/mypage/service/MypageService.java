32352248128e1ba9a08e7302dc08c3d0944fbf08
dohyeon5626 <kodohyeon71@gmail.com>
2021-09-29 09:26:25 +0900

    Add not normal user check code

--- src/main/java/com/example/portfolist/domain/mypage/service/MypageService.java
+++ src/main/java/com/example/portfolist/domain/mypage/service/MypageService.java
@@ -1,5 +1,6 @@
 package com.example.portfolist.domain.mypage.service;
 
+import com.example.portfolist.domain.auth.entity.NormalUser;
 import com.example.portfolist.domain.auth.entity.User;
 import com.example.portfolist.domain.auth.exception.PasswordNotMatchedException;
 import com.example.portfolist.domain.mypage.dto.request.PasswordChangeRequest;
@@ -14,16 +15,17 @@ public class MypageService {
 
     private final PasswordEncoder passwordEncoder;
 
-    public void changePassword(PasswordChangeRequest request, User user) {
-        if(!passwordEncoder.matches(request.getNowPassword(),user.getNormalUser().getPassword())) {
+    public void changePassword(PasswordChangeRequest request, NormalUser normalUser) {
+        if (!passwordEncoder.matches(request.getNowPassword(), normalUser.getPassword())) {
             throw new PasswordNotMatchedException();
         }
-        user.getNormalUser().setPassword(request.getNewPassword());
+        normalUser.setPassword(request.getNewPassword());
     }
 
-    public void checkPassword(PasswordCheckRequest request, User user) {
-        if(!passwordEncoder.matches(request.getNowPassword(),user.getNormalUser().getPassword())) {
+    public void checkPassword(PasswordCheckRequest request, NormalUser normalUser) {
+        if (!passwordEncoder.matches(request.getNowPassword(), normalUser.getPassword())) {
             throw new PasswordNotMatchedException();
         }
     }
+
 }
