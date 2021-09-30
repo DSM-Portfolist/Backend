bf58b0b0822d0c437f11765af2ca74da5bad7d8b
kwakdony <kwakdh25@gmail.com>
2021-09-30 16:08:49 +0900

    FIX

--- src/main/java/com/example/portfolist/domain/portfolio/repository/Container/BoxTextRepository.java
+++ src/main/java/com/example/portfolist/domain/portfolio/repository/Container/BoxTextRepository.java
@@ -1,7 +1,7 @@
-package com.example.portfolist.domain.portfolio.repository.experience;
+package com.example.portfolist.domain.portfolio.repository.Container;
 
 import com.example.portfolist.domain.portfolio.entity.container.BoxText;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExperienceTextRepository extends JpaRepository<BoxText,Long> {
+public interface BoxTextRepository extends JpaRepository<BoxText,Long> {
 }
