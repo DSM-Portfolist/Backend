4fff1ed08fc83bbcb05a167a1ee7ce6e0e6593e4
dohyeon5626 <kodohyeon71@gmail.com>
2021-09-30 09:23:31 +0900

    Change get baseurl code

--- src/main/java/com/example/portfolist/domain/auth/entity/NormalUser.java
+++ src/main/java/com/example/portfolist/domain/auth/entity/NormalUser.java
@@ -1,6 +1,9 @@
 package com.example.portfolist.domain.auth.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
