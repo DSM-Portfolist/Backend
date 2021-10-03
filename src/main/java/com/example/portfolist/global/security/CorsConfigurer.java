b2f26c16cf45499f65696f2b4de24a675b21cb1c
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-03 14:27:02 +0900

    Add security settings

---
+++ src/main/java/com/example/portfolist/global/security/CorsConfigurer.java
@@ -0,0 +1,16 @@
+package com.example.portfolist.global.security;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CorsConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        CorsFilter corsFilter = new CorsFilter();
+        http.addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
