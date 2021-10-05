fffe87705ef81a2381f423916b8c2b3cf30095d5
dohyeon5626 <kodohyeon71@gmail.com>
2021-10-05 18:44:37 +0900

    Add login service code test

--- src/test/java/com/example/portfolist/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/example/portfolist/domain/auth/service/AuthServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.portfolist.domain.auth.service;
 
 import com.example.portfolist.ServiceTest;
+import com.example.portfolist.domain.auth.dto.request.GithubUserLoginRequest;
 import com.example.portfolist.domain.auth.dto.request.NormalUserLoginRequest;
+import com.example.portfolist.domain.auth.dto.response.GithubUserLoginResponse;
 import com.example.portfolist.domain.auth.dto.response.NormalUserLoginResponse;
 import com.example.portfolist.domain.auth.entity.NormalUser;
 import com.example.portfolist.domain.auth.entity.User;
@@ -9,16 +11,22 @@ import com.example.portfolist.domain.auth.exception.PasswordNotMatchedException;
 import com.example.portfolist.domain.auth.repository.AuthCheckFacade;
 import com.example.portfolist.domain.auth.repository.AuthFacade;
 import com.example.portfolist.domain.auth.util.api.client.GithubClient;
+import com.example.portfolist.domain.auth.util.api.dto.GithubResponse;
 import com.example.portfolist.global.event.GlobalEventPublisher;
 import com.example.portfolist.global.mail.HtmlSourceProvider;
 import com.example.portfolist.global.security.JwtTokenProvider;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 public class AuthServiceTest extends ServiceTest {
@@ -44,64 +52,153 @@ public class AuthServiceTest extends ServiceTest {
     @InjectMocks
     private AuthService authService;
 
-    @Test
-    @DisplayName("Normal user login")
-    public void normalUserLogin() throws NoSuchFieldException, IllegalAccessException {
-        // given
-        NormalUserLoginRequest request = new NormalUserLoginRequest();
-        inputField(request, "email", "testtest@gmail.com");
-        inputField(request, "password", "testPassword");
+    @Nested
+    @DisplayName("Normal User Login")
+    class NormalUserLogin {
 
-        NormalUser normalUser = NormalUser.builder()
-                .email("testtest@gmail.com")
-                .password("testPassword")
-                .build();
+        private NormalUserLoginRequest makeRequest(String email, String password) throws NoSuchFieldException, IllegalAccessException {
+            NormalUserLoginRequest request = new NormalUserLoginRequest();
+            inputField(request, "email", email);
+            inputField(request, "password", password);
+            return request;
+        }
 
-        User user = User.builder()
-                .pk(1L)
-                .normalUser(normalUser)
-                .name("가나다")
-                .build();
+        @Test
+        @DisplayName("Success")
+        public void normalUserLogin_Success() throws NoSuchFieldException, IllegalAccessException {
+            // given
+            NormalUserLoginRequest request = makeRequest("testtest@gmail.com", "testPassword");
 
-        given(authCheckFacade.findByNormalUserEmail("testtest@gmail.com")).willReturn(user);
-        given(passwordEncoder.matches("testPassword","testPassword"))
-                .willReturn(true);
-        given(jwtTokenProvider.generateAccessToken(1L)).willReturn("accessToken");
-        given(jwtTokenProvider.generateRefreshToken(1L)).willReturn("refreshToken");
+            NormalUser normalUser = NormalUser.builder()
+                    .email("testtest@gmail.com")
+                    .password("testPassword")
+                    .build();
 
-        // when
-        NormalUserLoginResponse response = authService.login(request);
+            User user = User.builder()
+                    .pk(1L)
+                    .normalUser(normalUser)
+                    .name("가나다")
+                    .build();
 
-        // then
-        Assertions.assertEquals(response.getAccessToken(), "accessToken");
-        Assertions.assertEquals(response.getRefreshToken(), "refreshToken");
+            given(authCheckFacade.findByNormalUserEmail("testtest@gmail.com")).willReturn(user);
+            given(passwordEncoder.matches("testPassword", "testPassword"))
+                    .willReturn(true);
+            given(jwtTokenProvider.generateAccessToken(1L)).willReturn("accessToken");
+            given(jwtTokenProvider.generateRefreshToken(1L)).willReturn("refreshToken");
+
+            // when
+            NormalUserLoginResponse response = authService.login(request);
+
+            // then
+            Assertions.assertEquals(response.getAccessToken(), "accessToken");
+            Assertions.assertEquals(response.getRefreshToken(), "refreshToken");
+        }
+
+        @Test
+        @DisplayName("PasswordNotMatchedException")
+        public void normalUserLogin_PasswordNotMatchedException() throws NoSuchFieldException, IllegalAccessException {
+            // given
+            NormalUserLoginRequest request = makeRequest("testtest@gmail.com", "testWrongPassword");
+
+            NormalUser normalUser = NormalUser.builder()
+                    .email("testtest@gmail.com")
+                    .password("testPassword")
+                    .build();
+
+            User user = User.builder()
+                    .pk(1L)
+                    .normalUser(normalUser)
+                    .name("가나다")
+                    .build();
+
+            given(authCheckFacade.findByNormalUserEmail("testtest@gmail.com")).willReturn(user);
+            given(passwordEncoder.matches("testWrongPassword","testPassword"))
+                    .willReturn(false);
+
+            // when -> then
+            Assertions.assertThrows(PasswordNotMatchedException.class, () -> authService.login(request));
+
+        }
+
     }
 
-    @Test
-    @DisplayName("Normal user login - PasswordNotMatchedException")
-    public void normalUserLogin_PasswordNotMatchedException() throws NoSuchFieldException, IllegalAccessException {
-        // given
-        NormalUserLoginRequest request = new NormalUserLoginRequest();
-        inputField(request, "email", "testtest@gmail.com");
-        inputField(request, "password", "testPassword");
+    @Nested
+    @DisplayName("Github User Login")
+    class GithubUserLogin {
 
-        NormalUser normalUser = NormalUser.builder()
-                .email("testtest@gmail.com")
-                .password("testWrongPassword")
-                .build();
+        private GithubUserLoginRequest makeRequest(String githubToken) throws NoSuchFieldException, IllegalAccessException {
+            GithubUserLoginRequest request = new GithubUserLoginRequest();
+            inputField(request, "githubToken", githubToken);
+            return request;
+        }
 
-        User user = User.builder()
-                .pk(1L)
-                .normalUser(normalUser)
-                .name("가나다")
-                .build();
+        @DisplayName("First Login")
+        @ParameterizedTest(name = "{argumentsWithNames}")
+        @ValueSource(strings = { "name" })
+        @NullSource
+        public void githubUserFirstLogin(String name) throws NoSuchFieldException, IllegalAccessException {
+            // given
+            GithubUserLoginRequest request = makeRequest("githubToken");
 
-        given(authCheckFacade.findByNormalUserEmail("testtest@gmail.com")).willReturn(user);
-        given(passwordEncoder.matches("testPassword","testWrongPassword"))
-                .willReturn(false);
+            GithubResponse githubResponse = new GithubResponse();
+            inputField(githubResponse, "login", "nickname");
+            inputField(githubResponse, "avatarUrl", "profile");
+            inputField(githubResponse, "name", name);
+            given(githubClient.getUserInfo("token githubToken")).willReturn(githubResponse);
 
-        // when -> then
-        Assertions.assertThrows(PasswordNotMatchedException.class, () -> authService.login(request));
+            given(authFacade.findUserByGithubId("nickname")).willReturn(Optional.empty());
+            User user = User.builder()
+                    .pk(1L)
+                    .githubId("nickname")
+                    .name(name==null ? "nickname" : name)
+                    .url("profile")
+                    .build();
+            given(authFacade.save(any(User.class))).willReturn(user);
+
+            given(jwtTokenProvider.generateAccessToken(1L)).willReturn("accessToken");
+            given(jwtTokenProvider.generateRefreshToken(1L)).willReturn("refreshToken");
+
+            // when
+            GithubUserLoginResponse response = authService.login(request);
+
+            // then
+            Assertions.assertEquals(response.getAccessToken(), "accessToken");
+            Assertions.assertEquals(response.getRefreshToken(), "refreshToken");
+        }
+
+        @DisplayName("Login")
+        @ParameterizedTest(name = "{argumentsWithNames}")
+        @ValueSource(strings = { "name" })
+        @NullSource
+        public void githubUserLogin(String name) throws NoSuchFieldException, IllegalAccessException {
+            // given
+            GithubUserLoginRequest request = makeRequest("githubToken");
+
+            GithubResponse githubResponse = new GithubResponse();
+            inputField(githubResponse, "login", "nickname");
+            inputField(githubResponse, "avatarUrl", "profile");
+            inputField(githubResponse, "name", name);
+            given(githubClient.getUserInfo("token githubToken")).willReturn(githubResponse);
+
+            User user = User.builder()
+                    .pk(1L)
+                    .githubId("nickname")
+                    .name(name==null ? "nickname" : name)
+                    .url("profile")
+                    .build();
+            given(authFacade.findUserByGithubId("nickname")).willReturn(Optional.of(user));
+
+            given(jwtTokenProvider.generateAccessToken(1L)).willReturn("accessToken");
+            given(jwtTokenProvider.generateRefreshToken(1L)).willReturn("refreshToken");
+
+            // when
+            GithubUserLoginResponse response = authService.login(request);
+
+            // then
+            Assertions.assertEquals(response.getAccessToken(), "accessToken");
+            Assertions.assertEquals(response.getRefreshToken(), "refreshToken");
+        }
+
     }
 
 }
