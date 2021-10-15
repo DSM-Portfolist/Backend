4ec7b31c64d89847a0fb98836c5ab9861da3839e
kwakdony <kwakdh25@gmail.com>
2021-10-15 13:13:12 +0900

    Add querydslRepository
    리팩토링 필요

--- src/main/java/com/example/portfolist/domain/portfolio/repository/portfolio/PortfolioRepository.java
+++ src/main/java/com/example/portfolist/domain/portfolio/repository/portfolio/PortfolioRepository.java
@@ -4,8 +4,7 @@ import com.example.portfolist.domain.auth.entity.User;
 import com.example.portfolist.domain.portfolio.entity.portfolio.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long>{
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long>, QuerydslRepository {
 
     void deleteByUser(User user);
-
 }
