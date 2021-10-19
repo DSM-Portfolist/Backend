57f6a1178b7295dec15d9eaf5a4aeab0576cea81
kwakdony <kwakdh25@gmail.com>
2021-10-19 14:26:49 +0900

    Add error code

--- src/main/java/com/example/portfolist/global/error/ErrorCode.java
+++ src/main/java/com/example/portfolist/global/error/ErrorCode.java
@@ -11,12 +11,14 @@ public enum ErrorCode {
     WRONG_FILE(400, "Wrong File"),
     INVALID_TOKEN(401, "Invalid Token"),
     PASSWORD_NOT_MATCHED(401, "Password Not Matched"),
+    PERMISSION_DENIED(403, "Permission Denied"),
 
     USER_NOT_FOUND(404, "User Not Found"),
     NOT_NORMAL_USER(404, "Not Normal User"),
     USER_DUPLICATED(409, "User Duplicated"),
     EMAIL_NOT_AUTHORIZED(401, "Email Not Authorized"),
     FIELD_NOT_FOUND(404, "Field Not Found"),
+
     PORTFOLIO_NOT_FOUND(404, "Portfolio Not Found"),
     COMMENT_NOT_FOUND(404, "Comment Not Found");
 
