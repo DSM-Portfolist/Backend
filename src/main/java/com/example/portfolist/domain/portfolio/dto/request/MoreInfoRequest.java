a4f3eef39a06a266f5ce37ea4906f40cd47b2e8a
kwakdony <kwakdh25@gmail.com>
2021-10-27 18:32:23 +0900

    Add dto

---
+++ src/main/java/com/example/portfolist/domain/portfolio/dto/request/MoreInfoRequest.java
@@ -0,0 +1,11 @@
+package com.example.portfolist.domain.portfolio.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MoreInfoRequest {
+    private String name;
+    private String content;
+}
