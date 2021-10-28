d782318904e4d5956c13dc66097953ae47f12286
kwakdony <kwakdh25@gmail.com>
2021-10-28 11:19:19 +0900

    Rename dto field

--- src/main/java/com/example/portfolist/domain/portfolio/dto/request/PortfolioRequest.java
+++ src/main/java/com/example/portfolist/domain/portfolio/dto/request/PortfolioRequest.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -26,7 +25,7 @@ public class PortfolioRequest implements Serializable {
 
     private List<String> certificate;
 
-    private List<MoreInfoRequest> moreInfoRequestList;
+    private List<MoreInfoRequest> moreInfo;
 
     private String content;
 
