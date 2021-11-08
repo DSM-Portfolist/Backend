94f6713db57b206bd7ab40d005aa45b8639b39ca
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-08 09:07:21 +0900

    Change feign settings

--- src/main/java/com/example/portfolist/domain/auth/util/api/FeignClientErrorDecoder.java
+++ src/main/java/com/example/portfolist/domain/auth/util/api/FeignClientErrorDecoder.java
@@ -1,31 +1,28 @@
 package com.example.portfolist.domain.auth.util.api;
 
 import com.example.portfolist.domain.auth.exception.OauthServerException;
-import com.example.portfolist.domain.auth.util.api.dto.GithubErrorResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
-import feign.codec.StringDecoder;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
 
 @Slf4j
 public class FeignClientErrorDecoder implements ErrorDecoder {
-
-    private final StringDecoder stringDecoder = new StringDecoder();
+    
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public OauthServerException decode(final String methodKey, Response response) {
-        String message = "Server failed to request oauth server.";
+
+        String message = "Server failed to request other server.";
 
         if (response.body() != null) {
             try {
-                String json = stringDecoder.decode(response, String.class).toString();
-                message = objectMapper.readValue(json, GithubErrorResponse.class).getMessage();
-            } catch (IOException e) {
-                log.error(methodKey + "Error Deserializing response body from failed feign request response.", e);
+                message = objectMapper.readTree(response.body().toString())
+                        .get("message").asText();
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
             }
         }
 
--- src/main/java/com/example/portfolist/domain/auth/util/api/FeignConfiguration.java
+++ src/main/java/com/example/portfolist/domain/auth/util/api/FeignConfiguration.java
@@ -1,17 +1,10 @@
 package com.example.portfolist.domain.auth.util.api;
 
 import feign.Logger;
-import feign.codec.ErrorDecoder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableFeignClients(basePackages = "com.example.portfolist.domain.auth.util.api")
-@Import(FeignClientsConfiguration.class)
 public class FeignConfiguration {
 
     @Bean
@@ -20,7 +13,6 @@ public class FeignConfiguration {
     }
 
     @Bean
-    @ConditionalOnMissingBean(value = ErrorDecoder.class)
     public FeignClientErrorDecoder commonFeignErrorDecoder() {
         return new FeignClientErrorDecoder();
     }
