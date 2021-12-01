05c8a286abfa67e9b44e0a6a156773848fe28c02
kwakdony <kwakdh25@gmail.com>
2021-12-01 16:33:42 +0900

    exception handling

--- src/main/java/com/example/portfolist/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/portfolist/global/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.portfolist.domain.auth.exception.OauthServerException;
 import com.example.portfolist.global.error.exception.GlobalException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +16,7 @@ import org.springframework.web.multipart.support.MissingServletRequestPartExcept
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class, MissingServletRequestPartException.class,
-            MissingServletRequestParameterException.class, MultipartException.class})
+            MissingServletRequestParameterException.class, MultipartException.class, HttpMessageNotReadableException.class})
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final Exception e) {
         e.printStackTrace();
         final ErrorResponse response = new ErrorResponse(ErrorCode.INVALID_INPUT_VALUE);
