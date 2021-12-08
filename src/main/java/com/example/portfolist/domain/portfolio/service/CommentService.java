f716a2d56cca3fce8af7e1694e48d9502c4e61b9
kwakdony <kwakdh25@gmail.com>
2021-12-08 15:20:44 +0900

    Service 인터페이스 클래스 하나로 통일

--- src/main/java/com/example/portfolist/domain/portfolio/service/CommentService.java
+++ src/main/java/com/example/portfolist/domain/portfolio/service/CommentService.java
@@ -1,23 +1,122 @@
 package com.example.portfolist.domain.portfolio.service;
 
+import com.example.portfolist.domain.auth.entity.User;
+import com.example.portfolist.domain.mypage.entity.NoticeType;
 import com.example.portfolist.domain.portfolio.dto.request.ContentRequest;
 import com.example.portfolist.domain.portfolio.dto.response.CommentListResponse;
+import com.example.portfolist.domain.portfolio.dto.response.CommentResponse;
 import com.example.portfolist.domain.portfolio.dto.response.ReCommentResponse;
+import com.example.portfolist.domain.portfolio.entity.Portfolio;
+import com.example.portfolist.domain.portfolio.entity.comment.Comment;
+import com.example.portfolist.domain.portfolio.entity.comment.ReComment;
+import com.example.portfolist.domain.portfolio.exception.CommentNotFoundException;
+import com.example.portfolist.domain.portfolio.exception.PortfolioNotFoundException;
+import com.example.portfolist.domain.portfolio.repository.comment.CommentRepository;
+import com.example.portfolist.domain.portfolio.repository.comment.ReCommentRepository;
+import com.example.portfolist.domain.portfolio.repository.portfolio.PortfolioRepository;
+import com.example.portfolist.global.error.exception.PermissionDeniedException;
+import com.example.portfolist.global.event.GlobalEventPublisher;
+import com.example.portfolist.global.security.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface CommentService {
-    void createComment(long portfolioId, ContentRequest request);
+@Service
+@RequiredArgsConstructor
+public class CommentService {
 
-    void deleteComment(long commentId);
+    private final CommentRepository commentRepository;
+    private final PortfolioRepository portfolioRepository;
+    private final ReCommentRepository reCommentRepository;
 
-    void createReComment(long commentId, ContentRequest request);
+    private final AuthenticationFacade authenticationFacade;
 
-    void deleteReComment(long reCommentId);
+    private final GlobalEventPublisher eventPublisher;
 
-    CommentListResponse getCommentList(long portfolioId);
+    public CommentListResponse getCommentList(long portfolioId) {
 
-    List<ReCommentResponse> getReCommentList(long commentId);
+        List<CommentResponse> comments = commentRepository.findByPortfolioPk(portfolioId).stream()
+                .map(comment -> CommentResponse.of(comment, comment.getUser() != null && getCurrentUser().getPk() == comment.getUser().getPk()))
+                .collect(Collectors.toList());
 
-    void reportComment(long commentId, ContentRequest request);
+        return new CommentListResponse(comments);
+    }
+
+    public void createComment(long portfolioId, ContentRequest request) {
+        Portfolio portfolio = getPortfolio(portfolioId);
+        User user = getCurrentUser();
+        commentRepository.save(Comment.builder()
+                .portfolio(portfolio)
+                .user(user)
+                .date(LocalDate.now())
+                .deleteYN('N')
+                .content(request.getContent())
+                .build());
+        eventPublisher.makeNotice(user, portfolio.getUser(), NoticeType.COMMENT);
+    }
+
+    @Transactional
+    public void deleteComment(long commentId) {
+        Comment comment = getComment(commentId);
+
+        if (comment.getUser().getPk() == getCurrentUser().getPk()) {
+            if (comment.getReCommentList().size() == 0)
+                commentRepository.deleteById(commentId);
+            else
+                comment.deleteComment();
+        }
+        else
+            throw new PermissionDeniedException();
+    }
+
+    public List<ReCommentResponse> getReCommentList(long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(CommentNotFoundException::new);
+
+        return reCommentRepository.findAllByComment(comment).stream()
+                .map(reComment -> ReCommentResponse.of(reComment, getCurrentUser().getPk() == reComment.getUser().getPk()))
+                .collect(Collectors.toList());
+    }
+
+    public void createReComment(long commentId, ContentRequest request) {
+        Comment comment = getComment(commentId);
+        User user = getCurrentUser();
+        reCommentRepository.save(ReComment.builder()
+                .user(getCurrentUser())
+                .comment(getComment(commentId))
+                .date(LocalDate.now())
+                .content(request.getContent())
+                .build());
+        eventPublisher.makeNotice(user, comment.getUser(), NoticeType.RECOMMENT);
+    }
+
+    public void deleteReComment(long reCommentId) {
+        ReComment reComment = reCommentRepository.findById(reCommentId)
+                .orElseThrow(CommentNotFoundException::new);
+
+        if (reComment.getUser().getPk() == getCurrentUser().getPk())
+            reCommentRepository.deleteById(reCommentId);
+        else
+            throw new PermissionDeniedException();
+    }
+
+    public void reportComment(long commentId, ContentRequest request) {
+
+    }
+
+    private User getCurrentUser() {
+        return authenticationFacade.getUser();
+    }
+
+    private Portfolio getPortfolio(long portfolioId) {
+        return portfolioRepository.findById(portfolioId).orElseThrow(PortfolioNotFoundException::new);
+    }
+
+    private Comment getComment(long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(CommentNotFoundException::new);
+    }
 }
--- src/main/java/com/example/portfolist/domain/portfolio/service/TouchingService.java
+++ src/main/java/com/example/portfolist/domain/portfolio/service/TouchingService.java
@@ -1,6 +1,46 @@
 package com.example.portfolist.domain.portfolio.service;
 
-public interface TouchingService {
-    void createTouching(long portfolioId);
-    void deleteTouching(long portfolioId);
+import com.example.portfolist.domain.auth.entity.User;
+import com.example.portfolist.domain.mypage.entity.NoticeType;
+import com.example.portfolist.domain.portfolio.entity.Portfolio;
+import com.example.portfolist.domain.portfolio.entity.touching.Touching;
+import com.example.portfolist.domain.portfolio.entity.touching.TouchingId;
+import com.example.portfolist.domain.portfolio.exception.PortfolioNotFoundException;
+import com.example.portfolist.domain.portfolio.repository.TouchingRepository;
+import com.example.portfolist.domain.portfolio.repository.portfolio.PortfolioRepository;
+import com.example.portfolist.global.event.GlobalEventPublisher;
+import com.example.portfolist.global.security.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TouchingService {
+
+    private final TouchingRepository touchingRepository;
+    private final PortfolioRepository portfolioRepository;
+
+    private final AuthenticationFacade authenticationFacade;
+
+    private final GlobalEventPublisher eventPublisher;
+
+    public void createTouching(long portfolioId) {
+        Portfolio portfolio = getPortfolio(portfolioId);
+        User user = getCurrentUser();
+        touchingRepository.save(Touching.toEntity(user, portfolio));
+        eventPublisher.makeNotice(user, portfolio.getUser(), NoticeType.TOUCHING);
+    }
+
+    public void deleteTouching(long portfolioId) {
+        touchingRepository.deleteById(new TouchingId(getCurrentUser().getPk(), getPortfolio(portfolioId).getPk()));
+    }
+
+    private User getCurrentUser() {
+        return authenticationFacade.getUser();
+    }
+
+    private Portfolio getPortfolio(long portfolioId) {
+        return portfolioRepository.findById(portfolioId)
+                .orElseThrow(PortfolioNotFoundException::new);
+    }
 }
