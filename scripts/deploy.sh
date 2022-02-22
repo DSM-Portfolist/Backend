0d7b74bc44a418c99a3380f94c529da5046f0414
kwakdh25 <kwakdh25@gmail.com>
2022-02-22 19:42:07 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/action/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
 JAR_NAME=$(basename "$BUILD_JAR")
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/
+DEPLOY_PATH=/home/ec2-user/action/build/libs
 cp "$BUILD_JAR" $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
